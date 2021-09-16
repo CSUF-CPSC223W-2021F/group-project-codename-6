07272aa96eb4df19036245015fab6b23aa826b8a
Anthony Ngon <anthonyn5600@gmail.com>
2021-09-16 14:46:55 -0700

    Initial Commit

---
+++ ElectricStation/ElectricStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectricStationApp.swift
+//  ElectricStation
+//
+//  Created by csuftitan on 9/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ElectricStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
