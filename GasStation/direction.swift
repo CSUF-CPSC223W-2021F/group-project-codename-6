4d966d2828adef8b48b5c016b057d0d946ce2513
Anthony Ngon <37987501+anthonyn5600@users.noreply.github.com>
2021-10-04 12:54:53 -0700

    Revert "Revert "Main to display""

---
+++ GasStation/direction.swift
@@ -0,0 +1,13 @@
+//
+//  direction.swift
+//  GasStation
+//
+//  Created by csuftitan on 9/23/21.
+//  Anthony Ngon
+
+import Foundation
+
+struct Direction
+{
+  
+}
--- GasStationTests/GasStationTests.swift
+++ GasStationTests/GasStationTests.swift
@@ -1,12 +1,12 @@
 //
-//  ElectricStationTests.swift
-//  ElectricStationTests
+//  GasStationTests.swift
+//  GasStationTests
 //
 //  Created by csuftitan on 9/19/21.
 //
 
 import XCTest
-@testable import ElectricStation
+@testable import GasStation
 
 class ElectricStationTests: XCTestCase {
 
@@ -29,5 +29,4 @@ class ElectricStationTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
