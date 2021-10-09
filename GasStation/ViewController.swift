e813a7f29c4b355bc9f7b4fc3cd7f76c8861ef40
nebil <nebilgokdemir1@gmail.com>
2021-10-09 02:19:22 -0700

    add Current location to map

--- GasStation/ViewController.swift
+++ GasStation/ViewController.swift
@@ -8,14 +8,72 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
-    @IBOutlet weak var myMapView: MKMapView!
+    private var myLocationManager = CLLocationManager()
+    private var myCurrentLocation: CLLocationCoordinate2D?
+    @IBOutlet weak var myMapview: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationService()
     }
+    
+    
+    
+    
+    
+    func locationService()  {
+        myLocationManager.delegate = self
+        
+        
+            myMapview.showsUserLocation = true
+            myLocationManager.requestAlwaysAuthorization()
+            myLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+            myLocationManager.startUpdatingLocation()
 
-
+        }
+        
+        
+        
+        
+   
+        
+        
+    
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        
+        if let updateLocation = locations.first?.coordinate {
+            myCurrentLocation = updateLocation
+        }
+        
+        print(myCurrentLocation?.latitude)
+        print("my  longitude \(myCurrentLocation?.longitude)")
+    }
+   
+    @IBAction func nearGasStation(_ sender: UIButton) {
+    }
+    
+    @IBAction func cheapGasStation(_ sender: UIButton) {
+    
+    }
+    
+    
+    @IBAction func zoomIn(_ sender: UIButton) {
+        
+        print("zoom in")
+    }
+    
+    @IBAction func zoomOut(_ sender: UIButton) {
+        print("zoom out")
+    }
+    
+    
 }
 
