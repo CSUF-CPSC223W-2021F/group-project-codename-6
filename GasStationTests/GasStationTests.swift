55f4fd8867bafed4671d2f966a5bc9d2edf2f5d0
Anthony Ngon <37987501+anthonyn5600@users.noreply.github.com>
2021-10-25 15:21:53 -0700

    Added members unit tests and structure

--- GasStationTests/GasStationTests.swift
+++ GasStationTests/GasStationTests.swift
@@ -57,15 +57,62 @@ class GasStationTests: XCTestCase {
         gasData.shell.setPrice(price: 9)
         
         XCTAssertEqual(gasData.shell.getPrice(), 9)
-        
-        
-        
-        
-        
     }
     
     func testInput() {
         let test = userInput()
         XCTAssertEqual(test.input, "")
     }
+    
+    func testinit() {
+            let interface = userLocation(user: "Amritpaul Sidhu", userName: "Demonslaayer", location: "Cal State Fullerton")
+            XCTAssertEqual(interface.user, "Amritpaul Sidhu")
+            XCTAssertEqual(interface.userName, "Demonslaayer")
+            XCTAssertEqual(interface.location, "Cal State Fullerton")
+        }
+
+        func testUser() {
+            let interface = userLocation()
+            XCTAssertEqual(user1.firstName, "Amritpaul")
+            XCTAssertEqual(user1.lastName, "Sidhu")
+            XCTAssertEqual(user1.phoneNumber, 9099048124)
+            XCTAssertEqual(user1.birthDate, "10-22-2001")
+            XCTAssertEqual(user1.email, "asidhu2001@gmail.com")
+            XCTAssertEqual(interface.user, "")
+        }
+
+        func testLocation() {
+            let interface = userLocation()
+            XCTAssertEqual(gas3.address,  "327 beach st")
+            XCTAssertEqual(user1.homeAddress, "You dont need to know this")
+            XCTAssertEqual(user2.homeAddress, "12123 deex bin street")
+            XCTAssertEqual(gas1.coordinates,  ["Latitude": 11, "Longitude": 0])
+            XCTAssertEqual(interface.location, "")
+        }
+
+        func testUserName() {
+            let interface = userLocation()
+            XCTAssertEqual(gas5.name,  "76")
+            XCTAssertEqual(user1.userName, "DemonSlayer")
+            XCTAssertEqual(interface.userName, "")
+        }
+        
+        func testfindUser()
+        {
+            let interface = userLocation()
+            XCTAssertEqual(interface.findUser(), "")
+        }
+        
+        func testfindLocation()
+        {
+            let interface = userLocation()
+            XCTAssertEqual(interface.findLocation(), "")
+        }
+        
+        func testdisplayUserName()
+        {
+            let interface = userLocation()
+            XCTAssertEqual(interface.displayUserName(), "")
+        }
 }
+
