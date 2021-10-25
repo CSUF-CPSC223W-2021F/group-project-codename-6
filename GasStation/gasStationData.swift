1f43c419df5de659b636862a442fdfe1b81bde93
Anthony Ngon <37987501+anthonyn5600@users.noreply.github.com>
2021-10-24 20:17:26 -0700

    Fix some compatible issue, there is one more

--- GasStation/gasStationData.swift
+++ GasStation/gasStationData.swift
@@ -1,19 +1,19 @@
 //
-//  gasStationData.swift
-//  GasStation
+//  gasStationsData.swift
+//  gasStations
 //
 //  Created by nebil on 10/17/21.
 //
 
 import Foundation
 
-class GasStationData {
+class gasStationsData {
     
-    var stationData : [gasStation]
+    var stationData : [gasStations]
     
     init() {
         
-        stationData = [gasStation]()
+        stationData = [gasStations]()
         stationData.append(shell)
         stationData.append(arco)
         stationData.append(cosco)
@@ -22,26 +22,26 @@ class GasStationData {
         
         
     }
-    var shell = gasStation(title: "shell", latitude: 33.876236, longitude: -117.933154, price: 8, imageName: "shell.png")
+    var shell = gasStations(title: "shell", latitude: 33.876236, longitude: -117.933154, price: 8, imageName: "shell.png")
 
-    var arco = gasStation(title: "arco", latitude: 33.874731, longitude: -117.934920, price: 3, imageName: "argo.jpg")
-    var cosco = gasStation(title: "costco", latitude: 33.874856, longitude: -117.931862, price: 2, imageName: "costco.png")
-    var chevron = gasStation(title: "chevron", latitude: 33.865640, longitude: -117.947448, price: 7,imageName: "chevron")
+    var arco = gasStations(title: "arco", latitude: 33.874731, longitude: -117.934920, price: 3, imageName: "argo.jpg")
+    var cosco = gasStations(title: "costco", latitude: 33.874856, longitude: -117.931862, price: 2, imageName: "costco.png")
+    var chevron = gasStations(title: "chevron", latitude: 33.865640, longitude: -117.947448, price: 7,imageName: "chevron")
     
-    var mobile = gasStation(title: "mobile", latitude:  33.882784, longitude: -117.886305, price: 1, imageName:"")
+    var mobile = gasStations(title: "mobile", latitude:  33.882784, longitude: -117.886305, price: 1, imageName:"")
     
 
   
  
     
     
-    func getDtationData() -> [gasStation] {
+    func getDtationData() -> [gasStations] {
         
         return stationData
     }
     
     
-    func cheapest() -> gasStation {
+    func cheapest() -> gasStations {
         
        
         var myMax = stationData[0].price
