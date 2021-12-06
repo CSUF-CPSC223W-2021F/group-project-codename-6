90dcf1af32ef0e38e075d453ffc6979c38eefd9c
Anthony Ngon <37987501+anthonyn5600@users.noreply.github.com>
2021-12-05 18:37:53 -0800

    Revert "Nebil to Anthony"

--- GasStation/benmartinez.swift
+++ GasStation/benmartinez.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class benmartinez: UIViewController {
 
-    
-    @IBOutlet weak var regulerPrice: UITextField!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,24 +16,14 @@ class benmartinez: UIViewController {
     }
     
 
-    
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-        
-        if segue.identifier == "changePricesegue" {
-            let des = segue.destination as? ViewController
-            des!.changeRegulerPrice = 10
-        
-        }
     }
-    
-    @IBAction func changePrice(_ sender: UIButton) {
-        
-        
-    }
-    
+    */
+
 }
